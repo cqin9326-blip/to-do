f99a7e0181c87de72d392ea7d45b12803ee2d17e
cqin9326-blip <Cqin9326@gmail.com>
2026-06-12 19:02:33 -0500

    Create task input form

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,1 +1,33 @@
+import { useState } from 'react';
 
+// TaskForm lets the user type and submit a new task
+function TaskForm({ onAdd }) {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleSubmit = () => {
+        onAdd(inputValue);
+        setInputValue('');
+    };
+
+    const handleKeyDown = e => {
+        if (e.key === 'Enter') handleSubmit();
+    };
+
+    return (
+        <div className='task-form'>
+            <input
+                type='text'
+                className='task-input'
+                placeholder='New task'
+                value={inputValue}
+                onChange={e => setInputValue(e.target.value)}
+                onKeyDown={handleKeyDown}
+            />
+            <button className='btn-add' onClick={handleSubmit}>
+                Add
+            </button>
+        </div>
+    );
+}
+
+export default TaskForm;
