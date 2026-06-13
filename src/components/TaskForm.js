7e66d3d0ba8924e2b1d8d894ca651d2a2017c394
cqin9326-blip <Cqin9326@gmail.com>
2026-06-12 22:13:03 -0500

    everything done

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -4,30 +4,33 @@ import { useState } from 'react';
 function TaskForm({ onAdd }) {
     const [inputValue, setInputValue] = useState('');
 
-    const handleSubmit = () => {
-        onAdd(inputValue);
-        setInputValue('');
-    };
+    const handleSubmit = () => {
+        onAdd(inputValue);
+        setInputValue('');
+    };
 
-    const handleKeyDown = e => {
-        if (e.key === 'Enter') handleSubmit();
-    };
+    const handleKeyDown = e => {
+    if (e.key === 'Enter') handleSubmit();
+    };
 
-    return (
-        <div className='task-form'>
-            <input
-                type='text'
-                className='task-input'
-                placeholder='New task'
-                value={inputValue}
-                onChange={e => setInputValue(e.target.value)}
-                onKeyDown={handleKeyDown}
-            />
-            <button className='btn-add' onClick={handleSubmit}>
-                Add
-            </button>
-        </div>
-    );
+    return (
+        
+        <div className='task-form'>
+            <input
+                type='text'
+                className='task-input'
+                placeholder='New task'
+                value={inputValue}
+                onChange={e => setInputValue(e.target.value)}
+                onKeyDown={handleKeyDown}
+            />
+
+            <button className='btn-add' onClick={handleSubmit}>
+                Add
+            </button>
+
+        </div>
+    );
 }
 
 export default TaskForm;
